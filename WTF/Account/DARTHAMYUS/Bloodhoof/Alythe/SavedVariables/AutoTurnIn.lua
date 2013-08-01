67ccc6be149073f0b352c5fa726f7cb17bc966b6
Amy Elizabeth <jyetie@gmail.com>
2013-07-31 17:26:40 -0700

    a

---
+++ WTF/Account/DARTHAMYUS/Bloodhoof/Alythe/SavedVariables/AutoTurnIn.lua
@@ -0,0 +1,27 @@
+
+AutoTurnInCharacterDB = {
+	["togglekey"] = 4,
+	["questlevel"] = true,
+	["showrewardtext"] = true,
+	["watchlevel"] = true,
+	["enabled"] = true,
+	["todarkmoon"] = true,
+	["debug"] = false,
+	["secondary"] = {
+	},
+	["questshare"] = false,
+	["darkmoonautostart"] = true,
+	["darkmoonteleport"] = true,
+	["armor"] = {
+	},
+	["stat"] = {
+	},
+	["tournament"] = 2,
+	["version"] = "4.1",
+	["autoequip"] = false,
+	["lootreward"] = 1,
+	["all"] = false,
+	["trivial"] = false,
+	["weapon"] = {
+	},
+}
---
+++ WTF/Account/DARTHAMYUS/Bloodhoof/Alythe/SavedVariables/EveryQuest.lua
@@ -0,0 +1,16 @@
+
+EveryQuestDBPC = nil
+EQ2DBPC = {
+	["namespaces"] = {
+		["QuestGivers"] = {
+		},
+	},
+	["profileKeys"] = {
+		["Alythe - Bloodhoof"] = "global",
+	},
+	["profiles"] = {
+		["global"] = {
+			["dbversion"] = 4,
+		},
+	},
+}
